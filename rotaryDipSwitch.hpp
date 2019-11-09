96f6b27417e20814323cd034f846e1b49885d857
kouya17 <17383696+kouya17@users.noreply.github.com>
2019-11-09 17:42:13 +0900

    Add: rotaryDipSwitch

---
+++ rotaryDipSwitch.hpp
@@ -0,0 +1,25 @@
+/**
+ * @file rotaryDipSwitch.hpp
+ * @brief ロータリーDIPスイッチ用クラスヘッダ
+ */
+
+#ifndef ROTARY_DIP_SWITCH_HPP
+#define ROTARY_DIP_SWITCH_HPP
+
+#include <Arduino.h>
+
+/**
+ * @class RotaryDipSwitch
+ * @brief ロータリーDIPスイッチ用クラス
+ */
+
+static constexpr uint8_t 
+
+class RotaryDipSwitch {
+private:
+
+public:
+  RotaryDipSwitch(uint8_t[4] pin){}
+};
+
+#endif
